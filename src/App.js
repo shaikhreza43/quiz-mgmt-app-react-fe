7893c3cb52db80248c7bb75ce6e47776fde960c3
shaikhreza43 <reza.rk786@gmail.com>
2021-10-10 17:34:04 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useEffect } from 'react';
+import Auth from './components/Auth/Auth';
 
 function App() {
+
+  useEffect(() => {
+    window.onbeforeunload = confirmExit;
+    function confirmExit() {
+      return "Leave Site?";
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Redirect from="*" to="/auth" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
